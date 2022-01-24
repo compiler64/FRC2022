475938daad8b29932624673f125dada4354a29ab
LegoLefty <ajwarner005@gmail.com>
2022-01-23 21:19:59 -0500

    start working on the camera

---
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class Camera extends SubsystemBase {
+    public Camera() {
+        
+    }
+    public void display() {
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry ty = table.getEntry("ty");
+        NetworkTableEntry ta = table.getEntry("ta");
+
+        //read values periodically
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double area = ta.getDouble(0.0);
+
+        //post to shuffleboard periodically
+        SmartDashboard.putNumber("LimelightX", x);
+        SmartDashboard.putNumber("LimelightY", y);
+        SmartDashboard.putNumber("LimelightArea", area);
+    }
+}
--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -5,11 +5,9 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Pneumatics extends SubsystemBase {
-    // Compressor pcmcompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
     Solenoid testSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
-    //DoubleSolenoid solenoid12 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
     public Pneumatics() {
-        // pcmcompressor.enableDigital();
+        
     }
     public void setSingle(int port, Boolean value) {
         testSolenoid.set(value);
