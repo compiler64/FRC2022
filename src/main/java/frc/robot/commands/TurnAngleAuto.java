5bd66a542a436b27b0d0788fbd50fc72cfe66e69
Max Misterka <maxmisterka64@gmail.com>
2022-01-30 13:55:35 -0500

    Made DriveDistanceAuto and TurnAngleAuto accept DoubleSuppliers
    (to calculate distance on initialization instead of in the constructor)

--- src/main/java/frc/robot/commands/TurnAngleAuto.java
+++ src/main/java/frc/robot/commands/TurnAngleAuto.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Gyro;
@@ -10,6 +12,7 @@ import frc.robot.subsystems.Gyro;
 public class TurnAngleAuto extends CommandBase {
     private DriveTrain m_driveTrain;
     private Gyro m_gyro;
+    private DoubleSupplier m_angleSupplier;
     private double m_angle;
     private double m_speed;
 
@@ -29,8 +32,29 @@ public class TurnAngleAuto extends CommandBase {
         addRequirements(driveTrain, gyro);
     }
 
+    /**
+     * Creates a new TurnAngleAuto command.
+     * @param driveTrain the drive train of the robot
+     * @param gyro the gyro of the robot
+     * @param angle the angle to turn
+     * @param speed the factor to multiply the speed by
+     */
+    public TurnAngleAuto(DriveTrain driveTrain, Gyro gyro, DoubleSupplier angleSupplier, double speed) {
+        m_driveTrain = driveTrain;
+        m_gyro = gyro;
+        m_angleSupplier = angleSupplier;
+        m_speed = speed;
+
+        addRequirements(driveTrain, gyro);
+    }
+
     @Override
     public void initialize() {
+        // get the angle if a DoubleSupplier was passed to the constructor
+        if (m_angleSupplier != null) {
+            m_angle = m_angleSupplier.getAsDouble();
+        }
+
         // get the sign of the angle
         double sign = Math.signum(m_angle);
 
