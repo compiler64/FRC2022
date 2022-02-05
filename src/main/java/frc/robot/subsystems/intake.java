bac8013b0177090a66726a36d60e26f06bc44174
Max Misterka <maxmisterka64@gmail.com>
2022-02-05 09:56:28 -0500

    Working on intake code

--- src/main/java/frc/robot/subsystems/intake.java
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -1,7 +1,26 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.PortMap;
 
 public class intake extends SubsystemBase {
+    private TalonSRX m_motor_intake = new TalonSRX(PortMap.MOTOR_INTAKE_ID);
+    private TalonSRX m_motor_flywheel = new TalonSRX(PortMap.MOTOR_FLYWHEEL_ID);
+
     //TODO build the intake code
+
+    public intake() {
+
+    }
+
+    public void setIntakeSpeed(double speed) {
+        m_motor_intake.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void setFlywheelSpeed(double speed) {
+        m_motor_flywheel.set(ControlMode.PercentOutput, speed);
+    }
 }
