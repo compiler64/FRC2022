3ceb21a9fc7d5070b6b6b23f0d15796e39833e8e
LegoLefty <ajwarner005@gmail.com>
2022-02-06 11:42:36 -0500

    untested test closed loop control

--- src/main/java/frc/robot/commands/SparkTest.java
+++ src/main/java/frc/robot/commands/SparkTest.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Controllers;
 
+import edu.wpi.first.math.controller.PIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
@@ -11,20 +12,36 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class SparkTest extends CommandBase {
     CANSparkMax sparkMax = new CANSparkMax(13, MotorType.kBrushless);
+    SparkMaxPIDController pidController;
     
     RelativeEncoder encoder = sparkMax.getEncoder();
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
     public SparkTest() {
         Shuffleboard.getTab("main").addNumber("spark encoder",  () -> encoder.getVelocity());
-        SparkMaxPIDController pidController = sparkMax.getPIDController();
+        pidController = sparkMax.getPIDController();
+        kP = 6e-5; 
+        kI = 0;
+        kD = 0; 
+        kIz = 0; 
+        kFF = 0.000015; 
+        kMaxOutput = 1; 
+        kMinOutput = -1;
+        maxRPM = 5700;
+        pidController.setP(kP);
+        pidController.setI(kI);
+        pidController.setD(kD);
+        pidController.setIZone(kIz);
+        pidController.setFF(kFF);
+        pidController.setOutputRange(kMinOutput, kMaxOutput);
     }
 
     @Override
     public void execute() {
         if (Controllers.isButtonPressed(1, true)) {
-            sparkMax.set(1);
+            pidController.setReference(500, CANSparkMax.ControlType.kVelocity);
         }
         if (Controllers.isButtonReleased(1, true)) {
-            sparkMax.set(0);
+            pidController.setReference(0, CANSparkMax.ControlType.kVelocity);
         }
     }
 }
