d2ddb20daac4a5f5a4586a2f8cb8d305833d2e07
LegoLefty <ajwarner005@gmail.com>
2022-02-06 11:29:16 -0500

    start on closed loop control

--- src/main/java/frc/robot/commands/SparkTest.java
+++ src/main/java/frc/robot/commands/SparkTest.java
@@ -2,28 +2,31 @@ package frc.robot.commands;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Controllers;
 
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class SparkTest extends CommandBase {
     CANSparkMax sparkMax = new CANSparkMax(13, MotorType.kBrushless);
+    
     RelativeEncoder encoder = sparkMax.getEncoder();
     public SparkTest() {
-        Shuffleboard.getTab("main").addNumber("spark encoder",  () -> encoder.getVelocity());        
+        Shuffleboard.getTab("main").addNumber("spark encoder",  () -> encoder.getVelocity());
+        SparkMaxPIDController pidController = sparkMax.getPIDController();
     }
 
     @Override
     public void execute() {
         if (Controllers.isButtonPressed(1, true)) {
-            sparkMax.set(.99);
+            sparkMax.set(1);
         }
         if (Controllers.isButtonReleased(1, true)) {
             sparkMax.set(0);
