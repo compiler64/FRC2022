19538a9731eb03b7701baaa81a00fc95e92465a9
Max Misterka <maxmisterka64@gmail.com>
2022-02-12 10:54:38 -0500

    Made m_motor_belt be an SPX

--- src/main/java/frc/robot/commands/TestIntake.java
+++ src/main/java/frc/robot/commands/TestIntake.java
@@ -1,7 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Controllers;
 import frc.robot.PortMap;
