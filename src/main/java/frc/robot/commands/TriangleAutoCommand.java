f5ae96c9716b4a9f6a9e439d249beeda659b0a9a
LegoLefty <ajwarner005@gmail.com>
2022-02-19 11:35:06 -0500

    add a TwoBallAuto command

--- src/main/java/frc/robot/commands/TriangleAutoCommand.java
+++ src/main/java/frc/robot/commands/TriangleAutoCommand.java
@@ -4,6 +4,7 @@ import static frc.robot.Constants.*;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Camera;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Gyro;
@@ -19,11 +20,11 @@ public class TriangleAutoCommand extends SequentialCommandGroup {
     public TriangleAutoCommand(DriveTrain driveTrain, Gyro gyro, Pneumatics pneumatics, Camera camera, Intake intake, Shooter shooter) {
         Command[] commands = {
             new TurnFlywheel(shooter, AUTO_FLYWHEEL_SPEED),
-            // new DriveDistanceAuto(driveTrain, gyro, 1, AUTO_SPEED, true),
+            new WaitCommand(1),
             new ShootBall(shooter, INDEXING_WHEEL_SPEED, BALL_SHOOT_TIME),
             new DriveDistanceAuto(driveTrain, gyro, -1, AUTO_SPEED, true),
             new TurnAngleAuto(driveTrain, gyro, 140, AUTO_SPEED),
-            new DriveDistanceAuto(driveTrain, gyro, 7.5, AUTO_SPEED, false),
+            new DriveDistanceAuto(driveTrain, gyro, 6.5, AUTO_SPEED, false),
            
             new FollowBall(driveTrain, camera, AUTO_SPEED),
             new PickUpBall(intake, AUTO_INTAKE_SPEED, AUTO_BELT_SPEED, AUTO_INTAKE_TIME),
