f66139305d971e9f83a01c842c6d9bcc20ac4764
Max Misterka <maxmisterka64@gmail.com>
2022-02-26 12:04:33 -0500

    uncommented climber upper limit

--- src/main/java/frc/robot/commands/ClimberTeleop.java
+++ src/main/java/frc/robot/commands/ClimberTeleop.java
@@ -26,32 +26,26 @@ public class ClimberTeleop extends CommandBase {
     public void execute() {
         double leftStickY = Controllers.GetRawAxis(PortMap.XBOX_LS_Y, false);
         boolean aButton = Controllers.isButtonPressed(PortMap.XBOX_BUTTON_CLIMBER_ROTATE, false);
-        
-        
-        // boolean extenderAtUpperLimit = m_hanger.getExtenderPosition() >= CLIMBER_EXTENDER_UPPER_LIMIT;
+
+        boolean extenderAtUpperLimit = m_hanger.getExtenderPosition() >= CLIMBER_EXTENDER_UPPER_LIMIT;
         // boolean extenderAtLowerLimit = m_hanger.getExtenderPosition() <= CLIMBER_EXTENDER_LOWER_LIMIT;
-        
 
         if (Math.abs(leftStickY) < JOYSTICK_BUFFER) {
             leftStickY = 0;
         }
 
-        
         double leftPower = leftStickY * MOTOR_POWER_FACTOR;
         
         if (leftPower > 1 || leftPower < -1) {
             leftPower = 0;
         }
-        
 
         // if (extenderAtLowerLimit && leftPower < 0) {
         //     leftPower = 0;
         // }
-        // if (extenderAtUpperLimit && leftPower > 0) {
-        //     leftPower = 0;
-        // }
-
-        
+        if (extenderAtUpperLimit && leftPower > 0) {
+            leftPower = 0;
+        }
 
         m_hanger.setExtenderSpeed(leftPower * .5);
         
